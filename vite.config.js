5132a4ffe94e68fcbca279c75c289671eb448e88
jvdbreggen <juanb@redhat.com>
2026-08-24 14:14:32 +0100

    fixes from frontend for serviced

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 
+const hostname = os.hostname();
+const domain = hostname.split('.').slice(1).join('.');
+
 export default defineConfig({
   cacheDir: '/var/app/departures/.vite',
   server: {
     host: true,
     port: 5174,
-    allowedHosts: 'all',
+    allowedHosts: domain ? [hostname, `.${domain}`] : [hostname],
     proxy: {
+      '/bootc-status': {
+        target: 'http://localhost:8005',
+        rewrite: (path) => path.replace(/^\/bootc-status/, '/api/v1/status'),
+      },
       '/api': `http://${process.env.API_HOST || 'localhost'}:3001`,
     },
   },
