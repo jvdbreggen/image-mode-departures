7b4422f7b19f3d4afe74441f51cc7ca87a470552
jvdbreggen <juanb@redhat.com>
2026-08-24 12:49:32 +0100

    Fix train-tickets-departures.service for VM deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
-import os from 'os';
-
-const hostname = os.hostname();
-const domain = hostname.split('.').slice(1).join('.');
 
 export default defineConfig({
   cacheDir: '/var/app/departures/.vite',
   server: {
     host: true,
     port: 5174,
-    allowedHosts: domain ? [hostname, `.${domain}`] : [hostname],
+    allowedHosts: 'all',
     proxy: {
       '/api': `http://${process.env.API_HOST || 'localhost'}:3001`,
     },
